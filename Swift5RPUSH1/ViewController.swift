3387a632f8e092bb90bd3c5274ba751844ecffb5
kimura121212 <ykimuranoi@gmail.com>
2020-05-04 18:33:49 +0900

    first commit

---
+++ Swift5RPUSH1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5RPUSH1
+//
+//  Created by 木村友紀 on 2020/05/01.
+//  Copyright © 2020 木村友紀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
